06dbac6f02a412516293be53aa1d46ea291ddbc9
SturmEnte <sturmente@proton.me>
2022-08-28 02:42:49 +0200

    Added  board to GameData

--- src/game_data.rs
+++ src/game_data.rs
@@ -1,12 +1,15 @@
+use crate::board::Board;
+
 pub struct GameData {
-    team: u8,
+    pub team: u8,
+    pub board: Board,
 }
 
 impl GameData {
     pub fn new() -> GameData {
-        GameData { team: 0 }
+        GameData { team: 0, board: Board::new() }
     }
-
+    
     pub fn set_team(&mut self, team: &String) {
         if team == "ONE" {
             self.team = 1;
