3ff8da820adda878b95a26319f8f516be883a76c
SturmEnte <sturmente@proton.me>
2022-08-28 14:12:20 +0200

    Added start team and oppponent to GameData

--- src/game_data.rs
+++ src/game_data.rs
@@ -1,20 +1,32 @@
 use crate::board::Board;
 
 pub struct GameData {
-    pub team: u8,
     pub board: Board,
+    pub start_team: i8,
+    pub team: i8,
+    pub opponent: i8,
 }
 
 impl GameData {
     pub fn new() -> GameData {
-        GameData { team: 0, board: Board::new() }
+        GameData { team: 0, board: Board::new(), start_team: 0, opponent: 0 }
     }
     
     pub fn set_team(&mut self, team: &String) {
         if team == "ONE" {
             self.team = 1;
+            self.opponent = 2;
         } else {
+            self.opponent = 1;
             self.team = 2;
+        }
+    }
+
+    pub fn set_start_team(&mut self, team: &String) {
+        if team == "ONE" {
+            self.start_team = 1;
+        } else {
+            self.start_team = 2;
         }
     }
 }
